045b0525bf85cdc63bc178796c63e51c2ab994cb
sobameshi0901 <taptaptap0901@yahoo.co.jp>
2020-01-02 22:08:05 +0900

    practice_200102

---
+++ atcorder_problem/abc_140.cc
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+int main() {
+  int n;
+  cin >> n;
+  cout << pow(n, 3) << endl;
+}
+     
+
+#include <iostream>
+#include <vector>
+using namespace std;
+
+
+int main() {
+  int n;
+  cin >> n;
+  ans = 0;
+  vector<int> a;
+  vactor<int> b;
+  vector<int> c;
+  for(int i = 0; i < n; ++i) cin >> a[i];
+  for(int i = 0; i < n; ++i) {
+    cin >> b[i];
+    ans += b;
+  }
+  for(int i = 0; i < n; ++i) cin >> c[i];
+  for(int i = 0; i < n; ++i) {
+    if (a[i+1] == a[i]+1)ans+=c[i]
+  }
+}
+
